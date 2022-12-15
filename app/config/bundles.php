ed5a6a474f7654061bcea51eb851b6c2ad1ca3fb
Pau garcia <pau.verd11@gmail.com>
2022-12-15 16:30:18 +0100

    Symfony - Install Nelmio API DOC
    Install -> https://github.com/nelmio/NelmioApiDocBundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -3,4 +3,7 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
