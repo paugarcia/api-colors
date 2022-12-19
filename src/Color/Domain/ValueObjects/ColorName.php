097b7072390609a7776501d5f0018ea453a542e1
Pau garcia <pau.verd11@gmail.com>
2022-12-19 11:19:55 +0100

    DOMAIN - Add validation colorName
    Create different validation in valueObject ColorName

--- src/Color/Domain/ValueObjects/ColorName.php
+++ src/Color/Domain/ValueObjects/ColorName.php
@@ -2,11 +2,11 @@
 
 namespace Colors\Color\Domain\ValueObjects;
 
+use Colors\Color\Domain\Exceptions\InvalidColorNameEmptyException;
 use Colors\Color\Domain\Exceptions\InvalidColorNameException;
 
 final class ColorName
 {
-
     private string $name;
 
     public function __construct(string $name)
@@ -20,14 +20,17 @@ final class ColorName
         return $this->name;
     }
 
-    /**
-     * TODO: Add ColorName validations ( exercise rules )
-     */
     private function validate($name): void
     {
         if (empty($name)){
-            throw new InvalidColorNameException(
+            throw new InvalidColorNameEmptyException(
                 "Invalid color name, can't be empty."
+            );
+        }
+
+        if(!preg_match('/^[a-zA-Z0-9\-_\s]*$/', $name)){
+            throw new InvalidColorNameException(
+                "Invalid color name, can't have special characters."
             );
         }
     }
