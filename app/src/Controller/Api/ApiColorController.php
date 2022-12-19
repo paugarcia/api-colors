b0cf8dd8a1a9c23b6535797fc80e8365cbcb196d
Pau garcia <pau.verd11@gmail.com>
2022-12-19 10:05:15 +0100

    API - Initial Nelmio documentation

--- app/src/Controller/Api/ApiColorController.php
+++ app/src/Controller/Api/ApiColorController.php
@@ -2,38 +2,96 @@
 
 namespace App\Controller\Api;
 
+use Colors\Color\Infrastructure\ColorInMemoryRepository;
+
+use Colors\Color\Application\Commands\CreateColor\CreateColorApplicationService;
+use Colors\Color\Application\Commands\CreateColor\CreateColorCommand;
+use Colors\Color\Application\Commands\CreateColor\CreateColorCommandHandler;
+
 use Colors\Color\Application\Commands\DeleteColor\DeleteColorApplicationService;
 use Colors\Color\Application\Commands\DeleteColor\DeleteColorCommand;
 use Colors\Color\Application\Commands\DeleteColor\DeleteColorCommandHandler;
+
 use Colors\Color\Application\Queries\FindColorById\FindColorByIdApplicationService;
 use Colors\Color\Application\Queries\FindColorById\FindColorByIdQuery;
 use Colors\Color\Application\Queries\FindColorById\FindColorByIdQueryHandler;
-use Colors\Color\Infrastructure\ColorInMemoryRepository;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+
+use OpenApi\Attributes as OA;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/api/color")
  */
-
-/**
- * TODO: Make NelmioApi DOC in methods
- */
 class ApiColorController extends AbstractController
 {
     /**
-     * TODO: Make this method
+     * CREATE COLOR
+     *
+     * This call is used for create a color
      */
-    public function create()
-    {}
+    #[Route('/create/{colorId}', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Successful operation'
+    )]
+    #[OA\Response(
+        response: 400,
+        description: 'Error'
+    )]
+    #[OA\Tag(name: 'COLORS')]
+    public function create(string $colorId): JsonResponse
+    {
+        if (empty($colorId)){
+            return new JsonResponse(
+                ['message' => 'Missing parameters [colorId].'],
+                400
+            );
+        }
+
+        $colorRepository = new ColorInMemoryRepository();
+        $commandHandler = new CreateColorCommandHandler(new CreateColorApplicationService($colorRepository));
+
+        try {
+            /** TODO: CHANGE THIS AND CREATE A NEW ID */
+            $commandHandler->handle(new CreateColorCommand($colorId, $colorId));
+        } catch (\Exception $e){
+            return new JsonResponse(
+                ['message' => '[Error] ' . $e->getMessage()],
+                400
+            );
+        }
+
+        return new JsonResponse(
+            ['message' => 'The color (' . $colorId . ') has been created.'],
+            200
+        );
+    }
 
     /**
-     * @Route("/find/{colorId}", methods={"GET"})
+     * FIND COLOR
+     *
+     * This call is used for find a color
      */
+    #[Route('/find/{colorId}', methods: ['GET'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Color founded'
+    )]
+    #[OA\Response(
+        response: 404,
+        description: 'Color not found'
+    )]
+    #[OA\Response(
+        response: 400,
+        description: 'Error'
+    )]
+    #[OA\Tag(name: 'COLORS')]
     public function find(string $colorId):JsonResponse
     {
         if (empty($colorId)){
@@ -69,8 +127,20 @@ class ApiColorController extends AbstractController
     }
 
     /**
-     * @Route("/delete/{colorId}", methods={"DELETE","GET"})
+     * DELETE COLOR
+     *
+     * This call is used delete a color
      */
+    #[Route('/delete/{colorId}', methods: ['DELETE'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Color deleted'
+    )]
+    #[OA\Response(
+        response: 400,
+        description: 'Error'
+    )]
+    #[OA\Tag(name: 'COLORS')]
     public function delete(string $colorId): JsonResponse
     {
         if (empty($colorId)){
