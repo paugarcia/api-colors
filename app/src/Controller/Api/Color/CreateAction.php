42e07a81739bba1db1e36df12ed472ac63003998
Pau garcia <pau.verd11@gmail.com>
2022-12-19 15:58:58 +0100

    API - Change create route type

--- app/src/Controller/Api/Color/CreateAction.php
+++ app/src/Controller/Api/Color/CreateAction.php
@@ -4,6 +4,7 @@ namespace App\Controller\Api\Color;
 
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -22,7 +23,13 @@ final class CreateAction extends AbstractController {
      *
      * This call is used for create a color
      */
-    #[Route('/api/color/create/{colorId}', methods: ['GET'])]
+    #[Route('/api/color/create', methods: ['POST'])]
+    #[OA\Parameter(
+        name: 'color',
+        in: 'query',
+        description: 'The field used to order rewards',
+        schema: new OA\Schema(type: 'string')
+    )]
     #[OA\Response(
         response: 200,
         description: 'Successful operation'
@@ -32,21 +39,21 @@ final class CreateAction extends AbstractController {
         description: 'Error'
     )]
     #[OA\Tag(name: 'COLORS')]
-    public function create(string $colorId): JsonResponse
+    public function create(Request $request): JsonResponse
     {
-        if (empty($colorId)){
+        if (empty($request->get('color'))){
             return new JsonResponse(
-                ['message' => 'Missing parameters [colorId].'],
+                ['message' => 'Missing input [color].'],
                 400
             );
         }
 
         $colorRepository = new ColorInMemoryRepository();
         $commandHandler = new CreateColorCommandHandler(new CreateColorApplicationService($colorRepository));
-
+        $newColor = $request->get('color');
         try {
             /** TODO: CHANGE THIS AND CREATE A NEW ID */
-            $commandHandler->handle(new CreateColorCommand($colorId, $colorId));
+            $commandHandler->handle(new CreateColorCommand($newColor, $newColor));
         } catch (Exception $e){
             return new JsonResponse(
                 ['message' => '[Error] ' . $e->getMessage()],
@@ -55,7 +62,7 @@ final class CreateAction extends AbstractController {
         }
 
         return new JsonResponse(
-            ['message' => 'The color (' . $colorId . ') has been created.'],
+            ['message' => 'The color (' . $newColor . ') has been created.'],
             200
         );
     }
